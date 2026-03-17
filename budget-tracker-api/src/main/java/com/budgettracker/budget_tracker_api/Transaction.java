b4365c2f9b0fb5e77a676fd571498a4c145fff4e
kai0609-bit <kaiowada0609@gmail.com>
2026-03-17 15:05:22 +0000

    Update Transaction.java as Entity

--- budget-tracker-api/src/main/java/com/budgettracker/budget_tracker_api/Transaction.java
+++ budget-tracker-api/src/main/java/com/budgettracker/budget_tracker_api/Transaction.java
@@ -1,26 +1,31 @@
-package com.budgettracker;
+package com.budgettracker.budget_tracker_api;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Transaction {
-    // Properties
-    private String id;
+
+    @Id
+    @GeneratedValue
+    private long id;
     private String description;
     private double amount;
     private String category;
     private int month;
 
     // Constructors
-    Transaction (String id, String description, double amount, String category, int month) {
+    Transaction () {}
+
+    Transaction (String description, double amount, String category, int month) {
         if (amount == 0) {
             throw new IllegalArgumentException("Amount cannot be zero");
-        }
-        if (id == null || id.isBlank()) {
-            throw new IllegalArgumentException("ID is required");
         }
         if (description == null || description.isBlank()) {
             throw new IllegalArgumentException("Description is required");
         }
 
-        this.id = id;
         this.description = description;
         this.amount = amount;
         this.category = category;
@@ -28,22 +33,21 @@ public class Transaction {
     }
 
     // not using month
-    Transaction (String id, String description, double amount, String category) {
-        this(id, description, amount, category, 0);
+    Transaction (String description, double amount, String category) {
+        this(description, amount, category, 0);
     }
 
 
     // Getters
-    public String getId() {return id; }
+    public long getId() {return id; }
     public String getDescription() {return description; }
     public double getAmount() {return amount; }
     public String getCategory() {return category; }
     public int getMonth() {return month; }
 
-    @Override
-    public String toString() {
-        return String.format("Transaction{id='%s', description='%s', amount='%f', category='%s', month='%d'}",
-            id, description, amount, category, month);
-
-    }
+    // Setters
+    public void setDescription(String description) {this.description = description; }
+    public void setAmount(double amount) {this.amount = amount; }
+    public void setCategory(String category) {this.category = category; }
+    public void setMonth(int month) {this.month = month; }
 }
