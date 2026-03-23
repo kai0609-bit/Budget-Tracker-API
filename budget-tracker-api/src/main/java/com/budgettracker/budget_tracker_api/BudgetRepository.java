08b0e13eb7131c6b5a0ba8b7979e7e5436283552
kai0609-bit <kaiowada0609@gmail.com>
2026-03-23 18:33:07 +0000

    Update workflow and change file directory

--- budget-tracker-api/src/main/java/com/budgettracker/budget_tracker_api/BudgetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.budgettracker.budget_tracker_api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface BudgetRepository extends JpaRepository<Transaction, Long> {
-}
